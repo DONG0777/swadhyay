f01618043400118ff51b90dd3fbb7443ed50104a
DONG0777 <sukantashil07@gmail.com>
2026-08-14 22:29:14 +0530

    Deploy: Complete challenge fix with timer, XP, and auto-close

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "16d26eb519f4e9e3ea9a403f56480427
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "0df0e0af38c47b66dc37c8d83bba1e31",
+"flutter_bootstrap.js": "496de3b7c048e1ce94f95e245641ca03",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
