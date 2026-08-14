a5d1b1a1253f7bfb73b1a3dc97621588a4cbb030
DONG0777 <sukantashil07@gmail.com>
2026-08-14 23:14:33 +0530

    Fix: Deploy complete web build with all assets

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3589091122"
+    serviceWorkerVersion: "1235227931"
   }
 });
