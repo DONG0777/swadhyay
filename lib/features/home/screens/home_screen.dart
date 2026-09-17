467693dc13229cff0f5c4b477b08dd4873caee7c
DONG0777 <sukantashil07@gmail.com>
2026-09-18 01:08:45 +0530

    refactor(home): remove duplicate personal cards

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -7,16 +7,10 @@ import '../../admin/screens/admin_dashboard_screen.dart';
 import '../../admin/services/admin_service.dart';
 import '../../auth/services/auth_service.dart';
 import '../../community/screens/community_home_screen.dart';
-import '../../daily_swadhyay/screens/daily_commitment_screen.dart';
-import '../../daily_swadhyay/screens/daily_history_screen.dart';
-import '../../daily_swadhyay/screens/daily_reflection_screen.dart';
-import '../../daily_swadhyay/screens/growth_insight_screen.dart';
 import '../../daily_swadhyay/screens/swadhyay_hub_screen.dart';
-import '../../learning/screens/learning_screen.dart';
 import '../../profile/screens/profile_screen.dart';
 import '../../profile/services/profile_service.dart';
 import '../../surya_namaskar/screens/surya_namaskar_screen.dart';
-import '../../user_context/screens/user_context_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -73,45 +67,10 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  void _openLearning(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const LearningScreen(),
-      ),
-    );
-  }
 
-  void _openUserContext(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const UserContextScreen(),
-      ),
-    );
-  }
 
-  void _openDailyCommitment(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const DailyCommitmentScreen(),
-      ),
-    );
-  }
 
-  void _openDailyReflection(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const DailyReflectionScreen(),
-      ),
-    );
-  }
 
-  void _openDailyHistory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const DailyHistoryScreen(),
-      ),
-    );
-  }
 
   void _openCommunity(BuildContext context) {
     Navigator.of(context).push(
@@ -122,13 +81,6 @@ class HomeScreen extends StatelessWidget {
   }
 
 
-  void _openGrowthInsight(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (_) => const GrowthInsightScreen(),
-      ),
-    );
-  }
 
   void _openSwadhyayHub(BuildContext context) {
     Navigator.of(context).push(
@@ -258,150 +210,10 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openLearning(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.menu_book_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.learning,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.learningSubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openUserContext(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.explore_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.startMySwadhyay,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.startMySwadhyaySubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openDailyCommitment(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.flag_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.todaysCommitment,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.todaysCommitmentSubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openDailyReflection(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.self_improvement_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.nightReflection,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.nightReflectionSubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
+
+
+
+
                 Card(
                   child: InkWell(
                     borderRadius: BorderRadius.circular(12),
@@ -473,77 +285,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openGrowthInsight(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.auto_graph_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.growthInsight,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.growthInsightSubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Card(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(12),
-                    onTap: () => _openDailyHistory(context),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          const Icon(
-                            Icons.insights_outlined,
-                            size: 36,
-                          ),
-                          const SizedBox(width: 16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  strings.myJourney,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(strings.myJourneySubtitle),
-                              ],
-                            ),
-                          ),
-                          const Icon(Icons.chevron_right),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+
+
               ],
             ),
           ),
